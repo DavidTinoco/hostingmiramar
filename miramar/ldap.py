46a5dce3eab71fcd245729a12f77b98f0e9ddad7
David Tinoco <daviid.tc@gmail.com>
2018-01-11 14:41:54 +0100

    ldap add finalizado y search iniciado

--- miramar/ldap.py
+++ miramar/ldap.py
@@ -2,7 +2,7 @@ import ldap
 import ldap.modlist as modlist
 import commands
 
-def adduser(user,password,domain):
+def connect()
     # Open a connection
     l = ldap.initialize("ldap://172.22.200.146")
 
@@ -12,15 +12,19 @@ def adduser(user,password,domain):
     # The dn of our new entry/object
     dn="cn=prueba,dc=miramar,dc=gonzalonazareno,dc=org"
 
-    uidNumber = commands.getoutput("ldapsearch -H ldap://localhost -D "cn=Admin,dc=miramar,dc=gonzalonazareno,dc=org" -W | awk '/uidNumber: / {print $2}' | sort | tail -n 1")
+def adduser(user,password,domain):
+
+    connect
+
+    uidNumber = commands.getoutput("ldapsearch -H ldap://localhost -D "cn=admin,dc=miramar,dc=gonzalonazareno,dc=org" -w miramar | awk '/uidNumber: / {print $2}' | sort | tail -n 1")
 
     # A dict to help build the "body" of the object
     attrs = {}
     attrs['objectclass'] = ['top','posixAccount','inetOrgPerson','person']
     attrs['cn'] = user
     attrs['uid'] = user
-    attrs['uidNumber'] = uidNumber
-    attrs['gidNumber'] = uidNumber
+    attrs['uidNumber'] = int(uidNumber) + 1 
+    attrs['gidNumber'] = int(uidNumber) + 1
     attrs['homeDirectory'] = '/srv/ftp/' + user
     attrs['loginShell'] = '/bin/bash'
     attrs['userPassword'] = '{SSHA}yZ7g3Q2z7Ej1Ad3d3bia4fkKdqHWi4S0'
@@ -37,3 +41,13 @@ def adduser(user,password,domain):
 
     # Its nice to the server to disconnect and free resources when done
     l.unbind_s()
+
+def checkuser(user):
+
+        connect
+
+	existe = commands.getoutput("ldapsearch -H ldap://localhost -D "cn=Admin,dc=miramar,dc=gonzalonazareno,dc=org" -w miramar uid=" + user)
+	if existe == '':
+		return False
+	else
+		return True
